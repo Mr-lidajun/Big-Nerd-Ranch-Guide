40090072ed4f47bd0e69dca7cbf1ff94a7d22954
lidajun <541221507@qq.com>
2016-08-06 12:09:28 +0800

    第22章 深入学习intent和任务，初始化NerdLauncher应用（启动器）

---
+++ NerdLauncher/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.nerdlauncher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NerdLauncherActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nerd_launcher);
+    }
+}
